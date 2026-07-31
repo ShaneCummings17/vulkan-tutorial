404d303084bb7b2fb9e06e53dfe33d5c5de8ccfd
Shane Cummings <scummings36@gatech.edu>
2026-07-31 19:14:59 -0400

    -Set up CMake and Ninja to compile and run with C++ 26

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // C++26 Placeholder feature: using '_' multiple times in the same scope
+    int _ = 10;
+    std::string _ = "Hello C++26!";
+
+    std::cout << "Successfully compiled and executed using C++26 standard rules!" << std::endl;
+    return 0;
+}
